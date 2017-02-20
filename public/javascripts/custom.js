be1547572ad0be7741d5998dd9b0f33b599bbdc2
randriana <rina.andriana@outlook.com>
2017-02-20 15:44:00 +0100

    xs-devices looks okay now

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -7,3 +7,15 @@ $('.nav-link').click(function() {
 	$('#toggle').toggleClass('active');
   	$('#nav-overlay').toggleClass('open');
 });
+
+$('.toggle-info').click(function() {
+	$(this).toggleClass('active-info');
+});
+
+$(document).ready(function() {
+	var viewportWidth = $(window).width();
+
+	if( viewportWidth < 992) {
+		$('.toggle-info').addClass('active-info');
+	}
+});
