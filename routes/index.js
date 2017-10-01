121366d5f15e4b3cca3df47adfe920c539657114
randriana <rina.andriana@outlook.com>
2017-10-01 11:33:21 +0200

    Added webhook endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const VERIFY_TOKEN = 'MY_FITIA_TOKEN';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/health-check', (req, res) => res.sendStatus(200));
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VERIFY_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
 
 module.exports = router;
